6539689f4e995e8f567dc99eb5e6a1747e0914c8
jl2032 <55512607+jl2032@users.noreply.github.com>
2024-07-19 13:55:57 -0400

    Updated ALLOWED_HOSTS with site and development server URL

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://cryptic-shelf-71509-4e312cd92d34.herokuapp.com/']
 
 
 # Application definition
